75548bdceacf2c5981a3bb1609db0c6f3c1a1b3a
Jesus Fernandez <jemfernandez@fi.uba.ar>
2024-06-24 20:04:17 -0300

    Fixed date bug

--- backend/database.py
+++ backend/database.py
@@ -1,4 +1,5 @@
 import datetime as dt
+from sqlalchemy.sql import func
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
@@ -10,7 +11,7 @@ class Usuarios(db.Model):
     email = db.Column(db.String(255), unique=True, nullable=False)
     contrasenia = db.Column(db.String(20), nullable=False) # 20 caracteres como máximo
     rango = db.Column(db.String(15), nullable=False)
-    fecha_ingreso = db.Column(db.DateTime, default=dt.datetime.now())
+    fecha_ingreso = db.Column(db.DateTime(timezone=True), server_default=func.now())
 
 
 class Clientes(db.Model):
@@ -19,7 +20,7 @@ class Clientes(db.Model):
     nombre_cliente = db.Column(db.String(255), nullable=False)
     email = db.Column(db.String(255), unique=True, nullable=False)
     codigo_ingreso = db.Column(db.Integer, unique=True, nullable=False) # 8 dígitos y no se puede repetir
-    fecha_inscripcion = db.Column(db.DateTime, default=dt.datetime.now())
+    fecha_inscripcion = db.Column(db.DateTime(timezone=True), server_default=func.now())
 
 
 class Equipos(db.Model):
@@ -33,4 +34,4 @@ class Equipos(db.Model):
     observaciones = db.Column(db.String(500)) # Esto es para que el técnico le explique al cliente lo que pudo reparar y/o lo que no
     id_cliente = db.Column(db.Integer, db.ForeignKey("clientes.id"))
     id_tecnico = db.Column(db.Integer, db.ForeignKey("usuarios.id"))
-    fecha_ingreso = db.Column(db.DateTime, default=dt.datetime.now())
+    fecha_ingreso = db.Column(db.DateTime(timezone=True), server_default=func.now())
