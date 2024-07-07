ec5cc37576c211fbaec30d4154038678ff82e69a
Valentin Angrigiani <vangrigiani@fi.uba.ar>
2024-07-07 20:19:43 -0300

    Finished some details

--- backend/database.py
+++ backend/database.py
@@ -1,10 +1,9 @@
-from sqlalchemy.sql import func #es el remplazo del date time y funciono para resolver el problema con el horario adelantado por 2 horas y 10 minutos
+from sqlalchemy.sql import func # Es el reemplazo de datetime y funcionó para resolver el problema con el horario atrasado por 3 horas y 10 minutos
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
-# cada fila que tiene el atributo unique es que significa que hay un unico valor y que no puede haber mas valores iguales 
+# Cada fila que tiene el atributo 'unique' significa que hay un único valor y que no puede haber más valores iguales
 
-#en las tres tablas esta como primary key id(comentario en linea 27)
 class Usuarios(db.Model):
     __name__ = "usuarios"
     id = db.Column(db.Integer, primary_key=True)
@@ -12,8 +11,8 @@ class Usuarios(db.Model):
     email = db.Column(db.String(255), unique=True, nullable=False)
     contrasenia = db.Column(db.String(20), nullable=False) # 20 caracteres como máximo
     rango = db.Column(db.String(15), nullable=False)
-    #aca en este caso tuvimos que problemas con el horario 
-    #ya que la base de datos tomaba el horario de argentina en cambio phyton lo cual nos llevo a usar server_default
+    # Acá es donde tuvimos problemas con el horario, ya que la base de datos ahora toma el horario de Argentina.
+    # En cambio python no, lo cual nos llevo a usar el atributo server_default (Lo mismo está en las otras dos tablas)
     fecha_ingreso = db.Column(db.DateTime(timezone=True), server_default=func.now())
 
 class Clientes(db.Model):
@@ -24,7 +23,7 @@ class Clientes(db.Model):
     codigo_ingreso = db.Column(db.Integer, unique=True, nullable=False) # 8 dígitos y no se puede repetir
     fecha_inscripcion = db.Column(db.DateTime(timezone=True), server_default=func.now())
 
-#en esta tabla a diferencia de las otros usa dos foreignkey para las columnas id cliente y id tecnico
+# Esta tabla usa 2 foreign keys para las columnas id_cliente (relación con la tabla 'clientes') e id_tecnico (relación con la tabla 'usuarios')
 class Equipos(db.Model):
     __name__ = "equipos"
     id = db.Column(db.Integer, primary_key=True)
@@ -32,7 +31,7 @@ class Equipos(db.Model):
     marca = db.Column(db.String(255), nullable=False)
     modelo = db.Column(db.String(255), nullable=False)
     num_serie = db.Column(db.String(255), nullable=False)
-    estado = db.Column(db.String(30), nullable=False) # Nuevo Ingreso; En Revisión/Reparación; Reparado; No Reparado
+    estado = db.Column(db.String(30), nullable=False) # Nuevo Ingreso; En revisión/reparación; Reparado; No Reparado
     observaciones = db.Column(db.String(500)) # Esto es para que el técnico le explique al cliente lo que pudo reparar y/o lo que no
     id_cliente = db.Column(db.Integer, db.ForeignKey("clientes.id"))
     id_tecnico = db.Column(db.Integer, db.ForeignKey("usuarios.id"))
